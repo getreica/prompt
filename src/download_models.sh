1a1923436bc8bd0b2a91d830c2b6ddcbb235442a
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2025-01-11 16:36:12 +0100

    Update download_models.sh

--- src/download_models.sh
+++ src/download_models.sh
@@ -3,26 +3,53 @@
 # exit script if any command fails (non-zero value)
 set -e
 
-# Scarica i modelli solo se non presenti, usa variabile d'ambiente per il token
-echo "Verifica e download dei modelli se necessario..."
+echo "Startup..."
 
-# Esempio di download in diverse cartelle
-if [ ! -f "/all-models-downloaded.check" ]; then
-    echo "Download dei modelli..."
-    #
-    # Download weights - Da sostituire con i modelli che si vogliono scaricare
-    #
-    aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/sft/ae.sft https://huggingface.co/alexgenovese/vae/resolve/main/ae.sft
-    aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/gguf/flux1-dev-F16.gguf https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-F16.gguf
-    aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/gguf/t5-v1_1-xxl-encoder-f16.gguf https://huggingface.co/city96/t5-v1_1-xxl-encoder-gguf/resolve/main/t5-v1_1-xxl-encoder-f16.gguf
-    aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/safetensors/ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors https://huggingface.co/zer0int/CLIP-GmP-ViT-L-14/resolve/main/ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors
+# Cancellazione dei modelli
+if [[ $DELETE_MODELS ]]; then
+    echo "Eliminazione dei modelli..."
+    rm -rf comfyui/models
+    mkdir -p comfyui/models
+    rm -f /all-models-downloaded.check
+    echo "Eliminazione completata!"
+fi
+
+#
+# Download weights - Da sostituire con i modelli che si vogliono scaricare
+#
+if [[ $DOWNLOAD_MODELS ]]; then
+    if [ ! -f "/all-models-downloaded.check" ]; then
+        echo "Download dei modelli..."
+        # Scarica i modelli
+        if [ ! -f "comfyui/models/sft/ae.sft" ]; then
+            aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/sft/ae.sft https://huggingface.co/alexgenovese/vae/resolve/main/ae.sft
+        fi
+        if [ ! -f "comfyui/models/gguf/flux1-dev-F16.gguf" ]; then
+            aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/gguf/flux1-dev-F16.gguf https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-F16.gguf
+        fi
+        if [ ! -f "comfyui/models/gguf/t5-v1_1-xxl-encoder-f16.gguf" ]; then
+            aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/gguf/t5-v1_1-xxl-encoder-f16.gguf https://huggingface.co/city96/t5-v1_1-xxl-encoder-gguf/resolve/main/t5-v1_1-xxl-encoder-f16.gguf
+        fi
+        if [ ! -f "comfyui/models/safetensors/ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors" ]; then
+            aria2c --header="Authorization: Bearer $HUGGINGFACE_ACCESS_TOKEN" -o comfyui/models/safetensors/ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors https://huggingface.co/zer0int/CLIP-GmP-ViT-L-14/resolve/main/ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors
+        fi
+
+        # Crea il file di check
+        touch /all-models-downloaded.check
+        echo "Download completato!"
+    else
+        echo "I modelli sono già stati scaricati perchè c'è il lock file."
+    fi
+fi
     
-    # Crea il file di check
-    touch /all-models-downloaded.check
-    echo "Download completato!"
+echo "End startup."    
 
-else 
-    echo "Modelli già presenti, non è necessario scaricarli."
+if [[ $KEEP_OPEN ]]; then
+    echo "Il container è stato avviato con l'opzione --keep-open"
+
+    sleep infinity
+else
+    echo "Il container è stato avviato senza l'opzione --keep-open."
 fi
 
 # Avvia il comando principale del container
